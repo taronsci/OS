eda6b5ed457c49d6ced2c01cbb9a76e2047cc827
Taron Schisas <taron_schisas@edu.aua.am>
2025-09-09 18:02:37 +0400

    small change

--- HW1/Assignment_5.c
+++ HW1/Assignment_5.c
@@ -4,8 +4,8 @@ int main(void) {
     char str[] = "Hello";
     char *str1 = str;
 
-    for (int i = 0; *str1 != '\0'; i++, str1++) {
-        printf("%c", *(str + i));
+    for (; *str1 != '\0'; str1++) {
+        printf("%c", *str1)
     }
     printf("\n");
     printf("Length of string is %ld", str1 - str);
